399d21c71d6c31b09ff2574f26c6d0a179f87348
Aziz <abidaziz9876@gmail.com>
2023-05-20 14:37:00 +0530

    Final Commit

---
+++ app/src/main/java/com/example/shopster/Interfaces/ItemClickListener.java
@@ -0,0 +1,8 @@
+package com.example.shopster.Interfaces;
+
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void OnClick(View view, int position, boolean isLongClick);
+}
---
+++ app/src/main/java/com/example/shopster/ViewHolder/CartViewHolder.java
@@ -0,0 +1,34 @@
+package com.example.shopster.ViewHolder;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.shopster.Interfaces.ItemClickListener;
+import com.example.shopster.R;
+
+import org.jetbrains.annotations.NotNull;
+
+public class CartViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView cartProductName, cartProductPrice;
+    private ItemClickListener itemClickListener;
+
+    public CartViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+
+        cartProductName=itemView.findViewById(R.id.cart_product_name);
+        cartProductPrice=itemView.findViewById(R.id.cart_product_price);
+    }
+
+    @Override
+    public void onClick(View view) {
+        itemClickListener.OnClick(view,getAdapterPosition(),false);
+    }
+
+    public void setItemClickListener(ItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+}
